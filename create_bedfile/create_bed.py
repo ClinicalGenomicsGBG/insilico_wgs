8d50e0f3c46cb3cb0f90fd6b3feea13892b934e4
mathiasbio <math.joh.bio@gmail.com>
2020-09-24 09:29:50 +0200

    added an allowed chromlist to create of refseq dict because there exists alternative chromosomes in the refseq dict which can overwrite the real chromosomal positions, and that aint good

--- create_bedfile/create_bed.py
+++ create_bedfile/create_bed.py
@@ -11,10 +11,14 @@ def expand_regions(expand, start, stop):
 
 def create_refseq_dict(gtf, keep_cols_dict):
 	refseq_dict = {}
+	allowed_chromlist = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y", "MT", "M", "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY", "chrMT", "chrM"]
 	with open(gtf, 'r') as refgtf:
 		first_line = refgtf.readline().split("\t")
 		for transcript in refgtf:
 			transcriptlist = transcript.split("\t")
+			chrom = transcriptlist[keep_cols_dict["chrom"]]
+			if chrom not in allowed_chromlist:
+				continue
 			refseq_dict[transcriptlist[keep_cols_dict["name"]]] = {}
 			refseq_dict[transcriptlist[keep_cols_dict["name"]]]["transcript"] = transcriptlist[keep_cols_dict["name"]]
 			refseq_dict[transcriptlist[keep_cols_dict["name"]]]["gene"] = transcriptlist[keep_cols_dict["name2"]]
